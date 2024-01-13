472c3e99fe0a7dc2abc349cb08a848fa865742cc
Serveriii <n2jose00@students.oamk.fi>
2024-01-13 15:08:39 +0200

    ping

--- Mosh-tutorial/DoneWithIt/App.js
+++ Mosh-tutorial/DoneWithIt/App.js
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>How fast is it </Text>
+      <Text style={{fontSize: 25}}>{count}</Text>
+      <Button title="Click me" onPress={() => setCount(count+1)} color={"#004781"} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3b79d6',
     alignItems: 'center',
     justifyContent: 'center',
   },
