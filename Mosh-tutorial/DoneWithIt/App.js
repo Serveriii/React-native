89dfe492a2d91b3276a2f3ee4d121c98f2916984
Serveriii <n2jose00@students.oamk.fi>
2024-01-13 16:51:58 +0200

    Testing

--- Mosh-tutorial/DoneWithIt/App.js
+++ Mosh-tutorial/DoneWithIt/App.js
@@ -1,25 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import React, { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Platform
+} from "react-native";
+import { useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+
+
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>How fast is it </Text>
-      <Text style={{fontSize: 25}}>{count}</Text>
-      <Button title="Click me" onPress={() => setCount(count+1)} color={"#004781"} />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <View style={{
+      backgroundColor: "#ffffff",
+      flex: 1,
+    }}>
+      <View style={{
+        backgroundColor: "dodgerblue",
+        flex: 0.5,
+      }}/>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#3b79d6',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems:'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
