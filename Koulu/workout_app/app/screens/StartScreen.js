71fde9fcf1a2e8fcb7f6724aff07fff9cfd7bb6d
Serveriii <n2jose00@students.oamk.fi>
2024-01-25 10:15:40 +0200

    Styling

--- Koulu/workout_app/app/screens/StartScreen.js
+++ Koulu/workout_app/app/screens/StartScreen.js
@@ -1,33 +1,59 @@
 import React from "react";
-import { View, StyleSheet, StatusBar } from "react-native";
-import { Text, Button } from "react-native-paper";
-import { FontAwesome5 } from "@expo/vector-icons";
+import {
+  View,
+  Image,
+  StatusBar,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { Text, Button,  } from "react-native-paper";
+import { colors } from "../styles/mainStyles";
+import { startStyles } from "../styles/startScreenStyle";
 
-import { colors, containers, buttons } from "../styles/mainStyles";
 
 const menuButtons = [
   {
     label: "New workout",
-    navigate:"AddWork",
+    navigate: "AddWork",
     icon: "dumbbell",
+    image: require("../assets/jogging.jpg"),
   },
   {
     label: "Your workouts",
     navigate: "Workouts",
     icon: "format-list-bulleted",
+    image: require("../assets/monitor.jpg"),
   },
   {
     label: "Settings",
     navigate: "Settings",
     icon: "cog",
+    image: require("../assets/cogs.jpg"),
   },
 ];
 
 export default function StartScreen({ navigation }) {
   return (
-    <View style={containers.scaffold}>
+    <View style={startStyles.scaffold}>
       <StatusBar backgroundColor={colors.primary} barStyle="light-content" />
+
       {menuButtons.map((button, index) => (
+        <TouchableOpacity
+          style={startStyles.touchable}
+          onPress={() => navigation.navigate(button.navigate)}
+        >
+          <ImageBackground source={button.image} style={startStyles.image}>
+            <Text style={startStyles.touchtext}>{button.label}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+      ))}
+
+      {/* {menuButtons.map((button, index) => (
+        <TouchableRipple
+        children={<Image src={button.image}/>}
+        
+        />
+
         <Button
           key={index}
           onPress={() => navigation.navigate(button.navigate)}
@@ -38,7 +64,7 @@ export default function StartScreen({ navigation }) {
           style={buttons.button}
           labelStyle={{fontSize: 20}}
         />
-      ))}
+      ))} */}
     </View>
   );
 }
---
+++ Koulu/workout_app/app/styles/startScreenStyle.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+import { colors } from "./mainStyles";
+
+export const startStyles = StyleSheet.create({
+  scaffold: {
+    flex: 1,
+    backgroundColor: colors.dark,
+  },
+  container: {
+    flex: 0.3,
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  touchable: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  touchtext: {
+    fontSize: 40,
+    color: "white",
+  },
+});
