b255fb0a10d6d5b8b2f3b85be3ad5208c804b205
Serveriii <n2jose00@students.oamk.fi>
2024-02-22 10:05:17 +0200

    Bullshit buttons

--- Koulu/yahtzee/App.js
+++ Koulu/yahtzee/App.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import HomeScreen from "./screens/HomeScreen";
 import GameScreen from "./screens/GameScreen";
 import ScoreScreen from "./screens/ScoreScreen";
-
 
 const Stack = createBottomTabNavigator();
 
@@ -14,9 +14,37 @@ export default function App() {
     <PaperProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Game" component={GameScreen} />
-          <Stack.Screen name="Score" component={ScoreScreen} />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarLabelStyle: { color: "#4f1699" },
+              tabBarIcon: () => {
+                return <Icon name="home" size={30} color={"#4f1699"} />;
+              },
+            }}
+          />
+          <Stack.Screen
+            name="Game"
+            component={GameScreen}
+            options={{
+              tabBarLabelStyle: { color: "#4f1699" },
+              tabBarIcon: () => {
+                return <Icon name="dice" size={30} color={"#4f1699"} />;
+              },
+            }}
+          />
+          <Stack.Screen
+            name="Score"
+            component={ScoreScreen}
+            options={{
+              tabBarActiveTintColor: "red",
+              tabBarLabelStyle: { color: "#4f1699" },
+              tabBarIcon: () => {
+                return <Icon name="th-list" size={30} color={"#4f1699"} />;
+              },
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
--- Koulu/yahtzee/screens/HomeScreen.js
+++ Koulu/yahtzee/screens/HomeScreen.js
@@ -6,6 +6,7 @@ import { generalStyles } from "../styles/generalStyles";
 import { homeStyles } from "../styles/homeStyles";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import {dices} from "../data/Dices";
 
 export default function HomeScreen({ navigation}) {
   const [name, setName] = useState(null);
@@ -68,11 +69,27 @@ export default function HomeScreen({ navigation}) {
     <View style={generalStyles.scaffold}>
       <Header text={"Rules of the game"} />
       <View style={homeStyles.container}>
+        <Text>
+          THE GAME: Upper section of the classic Yahtzee dice game. You have{" "}
+          {dices.NBR_OF_DICES} dices and for the every dice you have {dices.NBR_OF_THROWS}
+          throws. After each throw you can keep dices in order to get same dice
+          spot counts as many as possible. In the end of the turn you must
+          select your points from {dices.MIN_SPOT} to {dices.MAX_SPOT}. Game ends when all
+          points have been selected. The order for selecting those is free.
+          POINTS: After each turn game calculates the sum for the dices you
+          selected. Only the dices having the same spot count are calculated.
+          Inside the game you can not select same points from
+          {dices.MIN_SPOT} to {dices.MAX_SPOT} again. GOAL: To get points as much as
+          possible.
+          {dices.BONUS_POINTS_LIMIT} points is the limit of getting bonus which gives
+          you {dices.BONUS_POINTS}
+          points more.
+        </Text>
         <Text>Good luck {name}</Text>
         <Button
-        children="Start game"
-        mode="contained"
-        onPress={() => navigation.navigate("Game")}
+          children="Start game"
+          mode="contained"
+          onPress={() => navigation.navigate("Game")}
         />
       </View>
       <Footer text={"Author: Severi Jokelainen"} />
