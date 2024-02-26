ae627c7622506a775f6ac22ac475fd809fecafb4
Serveriii <n2jose00@students.oamk.fi>
2024-02-26 16:30:15 +0200

    Gamescreen functions

--- Koulu/yahtzee/screens/GameScreen.js
+++ Koulu/yahtzee/screens/GameScreen.js
@@ -1,11 +1,47 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Pressable } from "react-native";
+import { Button } from "react-native-paper";
 import { generalStyles } from "../styles/generalStyles";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { dices } from "../data/Dices";
+
+let diceRow = [<Icon name="dice" size={60} />];
 
 export default function GameScreen() {
-    return (
-      <View style={generalStyles.scaffold}>
-        <Text>Game</Text>
-      </View>
-    );
+  const [gameDice, setGameDice] = useState([diceRow]);
+  const [outline, setOutline] = useState("-outline");
+
+  const startGame = () => {
+    diceRow = [];
+    for (let i = 0; i < dices.NBR_OF_DICES; i++) {
+      let random = Math.floor(Math.random() * 6) + 1;
+      diceRow.push(
+        <Pressable onPress={() => dicePress(i)}>
+          <MaterialCommunityIcons
+            name={"dice-" + [random] + outline}
+            size={50}
+            key={i}
+          />
+        </Pressable>
+      );
     }
+    setGameDice(diceRow);
+  };
+
+  const dicePress = (index) => {
+    console.log("Dice pressed: " + index);
+  };
+
+  return (
+    <View style={generalStyles.scaffold}>
+      <Header text={"Mini-yahtzee"} />
+      <View style={{ flexDirection: "row" }}>{gameDice}</View>
+      <Text>Throws left: {dices.NBR_OF_THROWS}</Text>
+      <Button children="Throw dices" mode="contained" onPress={startGame} />
+      <Footer text={"Author: Severi Jokelainen"} />
+    </View>
+  );
+}
