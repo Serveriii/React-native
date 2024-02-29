75ffcda2d61276ae2e4da424d8f8d9ded9ecd6f6
Serveriii <n2jose00@students.oamk.fi>
2024-02-29 13:09:05 +0200

    Choosing points works

--- Koulu/yahtzee/components/PointsRow.js
+++ Koulu/yahtzee/components/PointsRow.js
@@ -3,6 +3,7 @@ import { View, Text, Pressable } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { styles } from "../styles/gameStyles";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { dices } from "../data/Dices";
 
 const POINTS = 6;
 let numbers = [];
@@ -10,7 +11,9 @@ let numbers = [];
 export default function PointsRow(props) {
   const [counts, setCounts] = useState({});
   const [totalSum, setTotalSum] = useState(0);
-
+  const [pointState, setPointState] = useState(
+    new Array(dices.MAX_SPOT).fill(false)
+  );
   useEffect(() => {
     let numbers = props.board.map((item) => Number(item.match(/\d+/)[0]));
 
@@ -30,13 +33,29 @@ export default function PointsRow(props) {
     setCounts(countsObject);
   }, [props]);
 
-  const selectPoints = (i, counts) => {
-    console.log(Object.values(counts)[i]);
-    props.restart();
-    let sum = Object.values(counts)[i];
-    let newTotalSum = totalSum + sum;
-    setTotalSum(newTotalSum);
+  let counters = Object.entries(counts).map(([number, count]) => (
+    <Text key={number}>{`     ${count}   `}</Text>
+  ));
 
+  const selectPoints = (i, counts) => {
+    console.log(pointState[i]);
+    let newPointState = [...pointState];
+    newPointState[i] = true;
+    setPointState(newPointState);
+
+    
+    if (pointState[i] === false) {
+      let sum = Object.values(counts)[i];
+      let newTotalSum = totalSum + sum;
+      console.log(pointState[i]);
+
+      setTotalSum(newTotalSum);
+      setCounts({ ...counts, [i + 1]: sum });
+    } else {
+      alert("You have already selected this point.");
+    }
+
+    return newPointState[i];
     (async () => {
       try {
         await AsyncStorage.setItem("totalSum", totalSum.toString());
@@ -46,9 +65,12 @@ export default function PointsRow(props) {
     })();
   };
 
-  const pointColor = (i) => {
+  const pointColor = (i, color) => {
+    if (pointState[i] === true) {
+      return "purple";
+    }
     if (Object.values(counts)[i] === 0) {
-      return "black";
+      return color;
     } else {
       return "green";
     }
@@ -63,6 +85,7 @@ export default function PointsRow(props) {
           key={"row" + i}
           size={40}
           color={pointColor(i)}
+          state={pointState[i]}
         ></MaterialCommunityIcons>
       </Pressable>
     );
@@ -71,13 +94,7 @@ export default function PointsRow(props) {
   return (
     <View style={styles.container}>
       <Text>Total points: {totalSum}</Text>
-      <View style={styles.row}>
-        <Text>
-          {Object.entries(counts).map(([number, count]) => (
-            <Text key={number}>{`     ${count}   `}</Text>
-          ))}
-        </Text>
-      </View>
+      <View style={styles.row}>{counters}</View>
       <View style={styles.row}>{points}</View>
     </View>
   );
